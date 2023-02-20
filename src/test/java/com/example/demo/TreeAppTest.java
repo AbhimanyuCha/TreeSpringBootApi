3db18ab20c968bf4625429d2a767f658f248f785
Abhimanyu10 <abhimanyu19982002@gmail.com>
2023-02-21 02:08:07 +0530

    Implemented unit tests

---
+++ src/test/java/com/example/demo/TreeAppTest.java
@@ -0,0 +1,85 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class TreeAppTest {
+
+    @Mock
+    TreeService treeService;
+
+    private static final String badRequestBody = "Please Enter some values";
+    private static final int BAD_REQUEST_STATUS_CODE = 400;
+    private static final int OK_STATUS_CODE = 200;
+
+    @InjectMocks
+    TreeAppApplication treeAppApplication;
+
+    @BeforeEach
+    public void setup(){
+        treeService = mock(TreeServiceImpl.class);
+        treeAppApplication.treeService = this.treeService;
+    }
+
+    @Test
+    public void makeTreeControllerTest_withNullValuesPassed_shouldReturnBadRequest(){
+        ResponseEntity response = treeAppApplication.makeTreeController(Request.builder().values(null).build());
+        assertTrue(response.getStatusCode().equals(HttpStatusCode.valueOf(BAD_REQUEST_STATUS_CODE)));
+        assertTrue(response.getBody().equals(badRequestBody));
+    }
+
+    @Test
+    public void makeTreeControllerTest_withAlphaNumericValues_shouldThrowsNumberFormatException(){
+        doThrow(new NumberFormatException()).when(treeService).makeTree(any());
+        ResponseEntity response = treeAppApplication
+                .makeTreeController(Request
+                        .builder()
+                        .values(List.of("abc", "1", "2"))
+                        .build());
+        verify(treeService).makeTree(any());
+        assertEquals(response.getStatusCode(),HttpStatusCode.valueOf(BAD_REQUEST_STATUS_CODE));
+    }
+
+    @Test
+    public void makeTreeController_withValidValues_shouldReturnSuccess(){
+        ResponseEntity response = treeAppApplication
+                .makeTreeController(Request
+                        .builder()
+                        .values(List.of("1", "2", "3"))
+                        .build());
+        verify(treeService).makeTree(any());
+        assertTrue(response.getStatusCode().equals(HttpStatusCode.valueOf(OK_STATUS_CODE)));
+    }
+
+    @Test
+    public void deleteKeyController_withValidValue_shouldReturnSuccess(){
+        doNothing().when(treeService).makeTree(any(List.class));
+        treeAppApplication
+                .makeTreeController(Request
+                        .builder()
+                        .values(List.of("1","2","3","null","null","null","null"))
+                        .build());
+
+        verify(treeService).makeTree(any(List.class));
+
+        when(treeService.getRoots()).thenReturn(List.of(2,3));
+        ResponseEntity response = treeAppApplication.deleteNodeController("1");
+        assertTrue(response.getStatusCode().equals(HttpStatusCode.valueOf(OK_STATUS_CODE)));
+        assertEquals(response.getBody(),List.of(2,3));
+    }
+}
---
+++ src/test/java/com/example/demo/TreeServiceTest.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class TreeServiceTest {
+
+    TreeService treeService;
+
+    @BeforeEach
+    public void setup() {
+        treeService = new TreeServiceImpl();
+    }
+
+    @Test
+    public void makeTreeTest() {
+        treeService.makeTree(getNodesList());
+        assertTrue(treeService.getRoots().size() == 1);
+    }
+
+    @Test
+    public void deleteKey() {
+        treeService.makeTree(getNodesList());
+        treeService.deleteKey(1);
+        assertTrue(treeService.getRoots().size() == 2);
+        assertTrue(treeService.getRoots().get(0) == 2);
+        assertTrue(treeService.getRoots().get(1) == 3);
+    }
+
+    private List<String> getNodesList() {
+        return List.of("1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "null",
+                "6",
+                "null",
+                "null",
+                "null",
+                "null",
+                "7",
+                "null",
+                "null",
+                "null");
+    }
+
+
+}
